9594abc2bb39fbb15a7620a76715fc68df5ba2cc
Fatih-Akkaya <fatihakkaya@hotmail.com>
2021-01-20 21:20:46 +0300

    Müşteri Ekleme, Listeleme, Silme

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler, müşteri eklendi : " + musteri.Adi);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri kayıtlardan silindi : " + musteri.Adi);
+        }
+
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " Bakiye :" + musteri.Bakiye);                
+            }
+            Console.WriteLine("------------------");
+            Console.WriteLine("Tebrikler, Müşteriler listelendi ");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri[] musteriler = new Musteri[] { new Musteri() { Adi = "Fatih", Soyadi = "Akkaya", Bakiye = 100.5 },
+            new Musteri(){Adi="Furkan", Soyadi="Yılmaz", Bakiye=500.95 }, 
+            new Musteri(){Adi="Engin", Soyadi="Demiroğ", Bakiye=1250 } };
+            
+            Console.WriteLine("---------------Müşteri Ekleme-------------------");
+            MusteriManager musteriManager = new MusteriManager();
+            for (int i=0;i<musteriler.Length;i++)
+            {
+                musteriManager.Ekle(musteriler[i]);
+            }
+            Console.WriteLine("---------------Müşteri Listeleme-------------------");
+            musteriManager.Listele(musteriler);
+            Console.WriteLine("---------------Müşteri Silme-------------------");
+            musteriManager.Sil(musteriler[0]);
+            musteriManager.Sil(musteriler[1]);
+            
+        }
+    }
+}
