55e2e7b24bfc36b2262c81cdc2929001769ebde9
Fatih-Akkaya <fatihakkaya@hotmail.com>
2021-01-20 20:54:17 +0300

    Ref and Out Sample

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -6,19 +6,32 @@ namespace DegerVeReferansTipler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
 
             int sayi1 = 10;
             int sayi2 = 30;
-            sayi1 = sayi2;
-            sayi2 = 65;
-            Console.WriteLine(sayi1);
+            //sayi1 = sayi2;
+            //sayi2 = 65;
+            //Console.WriteLine(sayi1);
 
-            int[] sayilar1 = new int[] { 10, 20, 30 };
-            int[] sayilar2 = new int[] { 100, 200, 300 };
-            sayilar1 = sayilar2;
-            sayilar2[0] = 999;
-            Console.WriteLine(sayilar1[0]);
+            //int[] sayilar1 = new int[] { 10, 20, 30 };
+            //int[] sayilar2 = new int[] { 100, 200, 300 };
+            //sayilar1 = sayilar2;
+            //sayilar2[0] = 999;
+            //Console.WriteLine(sayilar1[0]);
+
+            RefAndOutSampleClass refAndOutSample = new RefAndOutSampleClass();
+            Console.WriteLine("Topla Methodu çalışmadan önce sayi2 : " + sayi2);
+            refAndOutSample.Topla(sayi1, ref sayi2);
+            Console.WriteLine("Topla Methodu çalıştıktan sonra sayi2 : " + sayi2);
+            int sayi3;
+
+            //Console.WriteLine("Çarpım Methodu çalışmadan önce sayi3 : " + sayi3);
+            refAndOutSample.Carpim(sayi2, out sayi3);
+            Console.WriteLine("Çarpım Methodu çalıştıktan sonra sayi3 : " + sayi3);
+            sayi3 = 8;
+            refAndOutSample.Carpim(sayi2, out sayi3);
+            Console.WriteLine("Çarpım Methodu 2. kez çalıştıktan sonra sayi3 : " + sayi3);
         }
     }
 }
---
+++ DegerVeReferansTipler/RefAndOutSampleClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DegerVeReferansTipler
+{
+    class RefAndOutSampleClass
+    {
+        public void Topla (int sayi1, ref int sayi2)
+        {
+            int toplam = sayi1 + sayi2;
+            sayi2 = toplam;
+            Console.WriteLine("Toplam Sonuç : " + toplam);
+        }
+        public void Carpim(int sayi1, out int sayi3)
+        {
+            sayi3 = 12;
+            int carpim = sayi1 * sayi3;
+            sayi3 = carpim;
+            Console.WriteLine("Çarpım Sonuç : " + carpim);
+        }
+    }
+}
