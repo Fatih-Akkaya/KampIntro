920e09c0d06cce49f3ac48aadca1565687d4f38f
Fatih-Akkaya <fatihakkaya@hotmail.com>
2021-01-20 23:49:12 +0300

    20.01.2021

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] {"Engin","Murat","Kerem","Halil" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+            //isimler[4] = "İlker"; //array ilk oluşturulduğunda 4 elemanlı olduğundan 5. eleman eklenememektedir
+            //Console.WriteLine(isimler[4]);
+
+            List<string> isimler2=new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("İlker"); //List koleksiyonuna ekleme yapabiliriz.
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product() { Id = 2, CategoryId = 5, UnitsInStock = 5, UnitPrice = 35, ProductName = "Kalem" };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            
+
+            //int,double,bool ... değer tip
+            //array, class, abstract class, interface,... referans tip
+        }
+    }
+}
